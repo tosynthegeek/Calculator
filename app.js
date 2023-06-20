7571fdaf49e38e2867cbf942d2a507ad760eba66
hyemiie <124187333+hyemiie@users.noreply.github.com>
2023-06-20 16:31:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const link = document.getElementById('currency');
+const elements = document.getElementsByClassName("currency_body");
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();  
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle('show'); 
+    elements[i].style.display = "block"; 
+  }
+});
+
+
+
+
+
+
+function calculate(){
+    const currency_one = currencyEl_one.value
+    const currency_two = currencyEl_two.value
+
+    fetch(`https://v6.exchangerate-api.com/v6/492ec17ebb60eacf7ff577aa/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+        // console.log(data);
+
+        const rate = data.conversion_rates[currency_two];
+        rateEl.innerText=`1 ${currency_one} =${rate} ${currency_two}`;
+
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+
+        swap.addEventListener('click' , () =>{
+            const temo = currencyEl_one.value;
+            currencyEl_two.value = currencyEl_one.value;
+            currencyEl_two.value=temp;
+            calculate();
+            
+
+        })
+    });
+}
+
+currencyEl_one.addEventListener('change', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+
+calculate();
+  
+
---
+++ calculator.js
@@ -0,0 +1,70 @@
+(function() {
+    let screen = document.querySelector('.screen input');
+    let buttons = document.querySelectorAll('.buttons .btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+  
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function(e) {
+        let value = e.target.dataset.num;
+        screen.value += value;
+      });
+    });
+  
+    equal.addEventListener('click', function(e) {
+      if (screen.value.trim() === "") {
+        screen.value = "Please enter";
+      } else {
+        try {
+          let result = evaluateExpression(screen.value);
+          screen.value = result;
+        } catch (error) {
+          screen.value = "Empty expression";
+        }
+      }
+    });
+  
+    clear.addEventListener('click', function(e) {
+      screen.value = "";
+    });
+  
+    function evaluateExpression(expression) {
+      const operators = {
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b
+      };
+  
+      const tokens = expression.split(/([+\-*/])/);
+  
+      let values = [];
+      let currentOperator = null;
+  
+      for (let token of tokens) {
+        if (token === "+" || token === "-" || token === "*" || token === "/") {
+          currentOperator = token;
+        } else {
+          let value = parseFloat(token);
+          if (isNaN(value)) {
+            throw new Error("Invalid expression");
+          }
+  
+          if (currentOperator !== null) {
+            let previousValue = values.pop();
+            let result = operators[currentOperator](previousValue, value);
+            values.push(result);
+            currentOperator = null;
+          } else {
+            values.push(value);
+          }
+        }
+      }
+  
+      if (values.length === 0) {
+        throw new Error("Invalid expression");
+      }
+  
+      return values[0];
+    }
+  })();
